c9a2eb5c4ea95b6d6918e00a26724cd16a78fd57
Joshua Madden <Joshua Madden>
2016-01-25 15:56:44 -0700

    finished 3 and 4

---
+++ 29/javaExercise4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Microwave {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter cook time: ");
+
+		numbers = scan.nextLine();
+		if (numbers.length < 3) {
+			System.out.println("Your Time: 0:" + numbers);
+		} else {
+			System.out.println("Your Time: " + )
+		}
+	}
+}
